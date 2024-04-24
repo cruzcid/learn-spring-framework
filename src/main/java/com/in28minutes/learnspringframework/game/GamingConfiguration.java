7f710f285e8402705c1424cdbb1fd69558474390
CruzInc <abstractime@outlook.com>
2024-04-24 16:41:21 -0600

    Use of field injection to inject beans

--- src/main/java/com/in28minutes/learnspringframework/game/GamingConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/game/GamingConfiguration.java
@@ -1,7 +1,4 @@
-package com.in28minutes.learnspringframework;
-
-import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.MarioGame;
+package com.in28minutes.learnspringframework.game;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
